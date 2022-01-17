eb00686b6daa651cedfcc6c8b72691a2d4ad4818
lb06 <lbcat@tutanota.com>
2022-01-17 03:08:57 +0100

    add more functions and conditions to mode

--- src/libbenten/src/lib.rs
+++ src/libbenten/src/lib.rs
@@ -70,12 +70,26 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test() {
+    fn test_japanese() {
         let mut engine = BentenEngine::new(BentenConfig { id: "japanese".to_string() });
 
+        //kana
         assert_eq!(engine.on_key_press(45), BentenResponse::Commit(String::from("い")));
         assert_eq!(engine.on_key_press(25), BentenResponse::Commit(String::from("く")));
+
+        //han
         assert_eq!(engine.on_key_press(47), BentenResponse::Empty);
-        assert_eq!(engine.on_key_press(18), BentenResponse::Commit(String::from("poop")));
+        assert_eq!(engine.on_key_press(18), BentenResponse::Suggest(String::from("日")));
+        assert_eq!(engine.on_key_press(65), BentenResponse::Commit(String::from("日")));
+
+        //back to kana
+        assert_eq!(engine.on_key_press(25), BentenResponse::Commit(String::from("く")));
+        assert_eq!(engine.on_key_press(65), BentenResponse::Null);
+
+        //han + next
+        assert_eq!(engine.on_key_press(47), BentenResponse::Empty);
+        assert_eq!(engine.on_key_press(18), BentenResponse::Suggest(String::from("日")));
+        assert_eq!(engine.on_key_press(23), BentenResponse::Suggest(String::from("曰")));
+        assert_eq!(engine.on_key_press(65), BentenResponse::Commit(String::from("曰")));
     }
 }
