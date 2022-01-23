a0b73b87182ffee073459141bb972ce31cb7d07d
lb06 <lbcat@tutanota.com>
2022-01-23 11:52:08 +0100

    object-ify benten_wayland for us with IPC

--- src/benten-wayland/src/lib.rs
+++ src/benten-wayland/src/lib.rs
@@ -4,7 +4,7 @@ use context::BentenContext;
 use mio::{unix::SourceFd, Events as MioEvents, Interest, Poll, Token};
 use mio_timerfd::{ClockId, TimerFd};
 
-use wayland_client::{ event_enum, Display, Filter, GlobalManager };
+use wayland_client::{ event_enum, Display, Filter, GlobalManager, EventQueue };
 use wayland_client::protocol::wl_seat::WlSeat;
 
 use wayland_protocols::misc::zwp_input_method_v2::client::{
@@ -13,8 +13,9 @@ use wayland_protocols::misc::zwp_input_method_v2::client::{
     zwp_input_method_keyboard_grab_v2::ZwpInputMethodKeyboardGrabV2,
 };
 
-
-use zwp_virtual_keyboard::virtual_keyboard_unstable_v1::zwp_virtual_keyboard_manager_v1::ZwpVirtualKeyboardManagerV1;
+use zwp_virtual_keyboard::virtual_keyboard_unstable_v1::{
+    zwp_virtual_keyboard_manager_v1::ZwpVirtualKeyboardManagerV1
+};
 
 event_enum! {
     Events |
@@ -22,114 +23,243 @@ event_enum! {
     Im => ZwpInputMethodV2
 }
 
-pub fn init() {
-    let display = Display::connect_to_env().map_err(|e| log::error!("Failed to connect to wayland display: {}", e)).unwrap();
-    let mut event_queue = display.create_event_queue();
-    let attached_display = display.attach(event_queue.token());
-    let globals = GlobalManager::new(&attached_display);
+pub struct Server {
+    pub context: BentenContext,
+    display: Display,
+    event_queue: EventQueue,
+    poll: Poll
+}
 
-    event_queue.sync_roundtrip(&mut (), |_, _, _| ()).unwrap();
+impl Server {
+    pub fn new(mode: &str) -> Self {
+        let display = Display::connect_to_env().map_err(|e| log::error!("Failed to connect to wayland display: {}", e)).unwrap();
+        let mut event_queue = display.create_event_queue();
+        let attached_display = display.attach(event_queue.token());
+        let globals = GlobalManager::new(&attached_display);
 
-    let seat = globals.instantiate_exact::<WlSeat>(1).expect("Failed to load Seat");
-    let im_manager = globals.instantiate_exact::<ZwpInputMethodManagerV2>(1).expect("Failed to load InputManager");
-    let vk_manager = globals.instantiate_exact::<ZwpVirtualKeyboardManagerV1>(1).expect("Failed to load VirtualKeyboardManager");
+        event_queue.sync_roundtrip(&mut (), |_, _, _| ()).unwrap();
 
-    let filter = Filter::new(|ev, _filter, mut data| {
-        let ctx = BentenContext::new_data(&mut data);
-        match ev {
-            Events::Key { event, .. } => {
-                ctx.handle_key_ev(event);
-            },
+        let seat = globals.instantiate_exact::<WlSeat>(1).expect("Failed to load Seat");
+        let im_manager = globals.instantiate_exact::<ZwpInputMethodManagerV2>(1).expect("Failed to load InputManager");
+        let vk_manager = globals.instantiate_exact::<ZwpVirtualKeyboardManagerV1>(1).expect("Failed to load VirtualKeyboardManager");
 
-            Events::Im { event, .. } => {
-                ctx.handle_im_ev(event);
+        let filter = Filter::new(|ev, _filter, mut data| {
+            let context = BentenContext::new_data(&mut data);
+            match ev {
+                Events::Key { event, .. } => {
+                    context.handle_key_ev(event);
+                },
+
+                Events::Im { event, .. } => {
+                    context.handle_im_ev(event);
+                }
             }
+        });
+
+        let vk = vk_manager.create_virtual_keyboard(&seat);
+        let im = im_manager.get_input_method(&seat);
+        let grab = im.grab_keyboard();
+        grab.assign(filter.clone());
+        im.assign(filter);
+
+        let mut timer = TimerFd::new(ClockId::Monotonic).expect("Initialize timer");
+        let mut poll = Poll::new().expect("Initialize epoll()");
+        
+        let registry = poll.registry();
+
+        const POLL_WAYLAND: Token = Token(0);
+        registry.register(
+            &mut SourceFd(&display.get_connection_fd()),
+            POLL_WAYLAND,
+            Interest::READABLE | Interest::WRITABLE,
+        ).expect("Register wayland socket to the epoll()");
+
+        // Required for hold event of engine values
+        const POLL_TIMER: Token = Token(1);
+        registry.register(
+            &mut timer, 
+            POLL_TIMER, 
+            Interest::READABLE
+        ).expect("Register timer to the epoll()");
+
+        // Initialize context
+        let mut context = BentenContext::new(mode, vk, im, timer);
+        event_queue.sync_roundtrip(&mut context, |_, _, _| ()).unwrap();
+        log::info!("Server successfully initialised !");
+
+        Self {
+            display,
+            event_queue,
+            context,
+            poll
         }
-    });
+    }
 
-    let vk = vk_manager.create_virtual_keyboard(&seat);
-    let im = im_manager.get_input_method(&seat);
-    let grab = im.grab_keyboard();
-    grab.assign(filter.clone());
-    im.assign(filter);
+    pub fn start(&mut self) {
+        let mut events = MioEvents::with_capacity(1024);
+        let stop_reason = 'main: loop {
+            use std::io::ErrorKind;
 
-    let mut timer = TimerFd::new(ClockId::Monotonic).expect("Initialize timer");
-    let mut poll = Poll::new().expect("Initialize epoll()");
-    
-    let registry = poll.registry();
+            // Sleep until next event
+            if let Err(e) = self.poll.poll(&mut events, None) {
+                // Should retry on EINTR
+                if e.kind() == ErrorKind::Interrupted {
+                    continue;
+                }
 
-    const POLL_WAYLAND: Token = Token(0);
-    registry.register(
-        &mut SourceFd(&display.get_connection_fd()),
-        POLL_WAYLAND,
-        Interest::READABLE | Interest::WRITABLE,
-    ).expect("Register wayland socket to the epoll()");
-
-    // Required for hold event of engine values
-    const POLL_TIMER: Token = Token(1);
-    registry.register(
-    	&mut timer, 
-    	POLL_TIMER, 
-    	Interest::READABLE
-    ).expect("Register timer to the epoll()");
-
-    // Initialize context
-    let mut ctx = BentenContext::new(vk, im, timer);
-    event_queue.sync_roundtrip(&mut ctx, |_, _, _| ()).unwrap();
-    log::info!("Server successfully initialised !");
-
-    // Non-blocking event loop
-    let mut events = MioEvents::with_capacity(1024);
-    let stop_reason = 'main: loop {
-        use std::io::ErrorKind;
-
-        // Sleep until next event
-        if let Err(e) = poll.poll(&mut events, None) {
-            // Should retry on EINTR
-            if e.kind() == ErrorKind::Interrupted {
-                continue;
+                break Err(e);
             }
 
-            break Err(e);
-        }
+            for event in &events {
+                match event.token() {
+                    POLL_WAYLAND => {}
+                    POLL_TIMER => {
+                        if let Err(e) = self.context.handle_timer_ev() {
+                            break 'main Err(e);
+                        }
+                    }
+                    _ => unreachable!(),
+                }
+            }
 
-        for event in &events {
-            match event.token() {
-                POLL_WAYLAND => {}
-                POLL_TIMER => {
-                    if let Err(e) = ctx.handle_timer_ev() {
-                        break 'main Err(e);
+            // Perform read() only when it's ready, returns None when there're already pending events
+            if let Some(guard) = self.event_queue.prepare_read() {
+                if let Err(e) = guard.read_events() {
+                    // ErrorKind::WouldBlock here means there's no new messages to read
+                    if e.kind() != ErrorKind::WouldBlock {
+                        break Err(e);
                     }
                 }
-                _ => unreachable!(),
             }
-        }
 
-        // Perform read() only when it's ready, returns None when there're already pending events
-        if let Some(guard) = event_queue.prepare_read() {
-            if let Err(e) = guard.read_events() {
-                // ErrorKind::WouldBlock here means there's no new messages to read
+            if let Err(e) = self.event_queue.dispatch_pending(&mut self.context, |_, _, _| {}) {
+                break Err(e);
+            }
+
+            // Flush pending writes
+            if let Err(e) = self.display.flush() {
+                // ErrorKind::WouldBlock here means there're so many to write, retry later
                 if e.kind() != ErrorKind::WouldBlock {
                     break Err(e);
                 }
             }
-        }
+        };
 
-        if let Err(e) = event_queue.dispatch_pending(&mut ctx, |_, _, _| {}) {
-            break Err(e);
+        match stop_reason {
+            Ok(()) => log::info!("Server closed gracefully"),
+            Err(e) => log::error!("Server aborted: {}", e),
         }
-
-        // Flush pending writes
-        if let Err(e) = display.flush() {
-            // ErrorKind::WouldBlock here means there're so many to write, retry later
-            if e.kind() != ErrorKind::WouldBlock {
-                break Err(e);
-            }
-        }
-    };
-
-    match stop_reason {
-        Ok(()) => log::info!("Server closed gracefully"),
-        Err(e) => log::error!("Server aborted: {}", e),
     }
 }
+
+// pub fn init(mode: &str) {
+    // let display = Display::connect_to_env().map_err(|e| log::error!("Failed to connect to wayland display: {}", e)).unwrap();
+    // let mut event_queue = display.create_event_queue();
+    // let attached_display = display.attach(event_queue.token());
+    // let globals = GlobalManager::new(&attached_display);
+
+    // event_queue.sync_roundtrip(&mut (), |_, _, _| ()).unwrap();
+
+    // let seat = globals.instantiate_exact::<WlSeat>(1).expect("Failed to load Seat");
+    // let im_manager = globals.instantiate_exact::<ZwpInputMethodManagerV2>(1).expect("Failed to load InputManager");
+    // let vk_manager = globals.instantiate_exact::<ZwpVirtualKeyboardManagerV1>(1).expect("Failed to load VirtualKeyboardManager");
+
+    // let filter = Filter::new(|ev, _filter, mut data| {
+    //     let ctx = BentenContext::new_data(&mut data);
+    //     match ev {
+    //         Events::Key { event, .. } => {
+    //             ctx.handle_key_ev(event);
+    //         },
+
+    //         Events::Im { event, .. } => {
+    //             ctx.handle_im_ev(event);
+    //         }
+    //     }
+    // });
+
+    // let vk = vk_manager.create_virtual_keyboard(&seat);
+    // let im = im_manager.get_input_method(&seat);
+    // let grab = im.grab_keyboard();
+    // grab.assign(filter.clone());
+    // im.assign(filter);
+
+    // let mut timer = TimerFd::new(ClockId::Monotonic).expect("Initialize timer");
+    // let mut poll = Poll::new().expect("Initialize epoll()");
+    
+    // let registry = poll.registry();
+
+    // const POLL_WAYLAND: Token = Token(0);
+    // registry.register(
+    //     &mut SourceFd(&display.get_connection_fd()),
+    //     POLL_WAYLAND,
+    //     Interest::READABLE | Interest::WRITABLE,
+    // ).expect("Register wayland socket to the epoll()");
+
+    // // Required for hold event of engine values
+    // const POLL_TIMER: Token = Token(1);
+    // registry.register(
+    // 	&mut timer, 
+    // 	POLL_TIMER, 
+    // 	Interest::READABLE
+    // ).expect("Register timer to the epoll()");
+
+    // // Initialize context
+    // let mut ctx = BentenContext::new(mode, vk, im, timer);
+    // event_queue.sync_roundtrip(&mut ctx, |_, _, _| ()).unwrap();
+    // log::info!("Server successfully initialised !");
+
+    // // Non-blocking event loop
+    // let mut events = MioEvents::with_capacity(1024);
+    // let stop_reason = 'main: loop {
+    //     use std::io::ErrorKind;
+
+    //     // Sleep until next event
+    //     if let Err(e) = poll.poll(&mut events, None) {
+    //         // Should retry on EINTR
+    //         if e.kind() == ErrorKind::Interrupted {
+    //             continue;
+    //         }
+
+    //         break Err(e);
+    //     }
+
+    //     for event in &events {
+    //         match event.token() {
+    //             POLL_WAYLAND => {}
+    //             POLL_TIMER => {
+    //                 if let Err(e) = ctx.handle_timer_ev() {
+    //                     break 'main Err(e);
+    //                 }
+    //             }
+    //             _ => unreachable!(),
+    //         }
+    //     }
+
+    //     // Perform read() only when it's ready, returns None when there're already pending events
+    //     if let Some(guard) = event_queue.prepare_read() {
+    //         if let Err(e) = guard.read_events() {
+    //             // ErrorKind::WouldBlock here means there's no new messages to read
+    //             if e.kind() != ErrorKind::WouldBlock {
+    //                 break Err(e);
+    //             }
+    //         }
+    //     }
+
+    //     if let Err(e) = event_queue.dispatch_pending(&mut ctx, |_, _, _| {}) {
+    //         break Err(e);
+    //     }
+
+    //     // Flush pending writes
+    //     if let Err(e) = display.flush() {
+    //         // ErrorKind::WouldBlock here means there're so many to write, retry later
+    //         if e.kind() != ErrorKind::WouldBlock {
+    //             break Err(e);
+    //         }
+    //     }
+    // };
+
+    // match stop_reason {
+    //     Ok(()) => log::info!("Server closed gracefully"),
+    //     Err(e) => log::error!("Server aborted: {}", e),
+    // }
+// }
