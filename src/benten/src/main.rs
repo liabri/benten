ebdc119d688a350786db07929c8861229cbd031f
lb06 <lbcat@tutanota.com>
2022-01-25 15:09:35 +0100

    on launch read current_mode file

--- src/benten/src/main.rs
+++ src/benten/src/main.rs
@@ -1,26 +1,16 @@
 extern crate benten_wayland;
 mod logger;
 
-// use inotify::{ Inotify, WatchMask };
-// use std::thread;
+use std::fs::read_to_string;
 
 fn main() {
-    logger::init("debug").map_err(|err| eprintln!("logger failed to initialise: {:?}", err)).unwrap();
-   
+	logger::init("debug").map_err(|err| eprintln!("logger failed to initialise: {:?}", err)).unwrap();
 	let path = xdg::BaseDirectories::with_prefix("benten").unwrap().get_data_home().join("current_mode");
-    let mut state = benten_wayland::State::new(&path, "kana");
-    state.run();
 
-	// std::thread::spawn(|| {
-	// 	let mut watcher = Inotify::init().unwrap();
-	    // let path = xdg::BaseDirectories::with_prefix("benten").unwrap().get_data_home().join("current_mode");
-	// 	watcher.add_watch(&path, WatchMask::MODIFY).unwrap();
-	// 	let mut buffer = [0; 1024];
+	//read mode at $XDG_DATA_HOME/benten/current_mode, if not set panic
+	let mode = read_to_string(&path)
+		.map_err(|_| log::error!("No mode set at $XDG_DATA_HOME/benten/current_mode")).unwrap();
 
-	// 	let events = watcher.read_events_blocking(&mut buffer).unwrap();
-	// 	for event in events {
-	// 	    println!("event: {:?}", event);
-	// 	    server.context.engine.set_mode("cangjie5"); //value of read_to_string() eventually
-	// 	}
- //    });
+	let mut state = benten_wayland::State::new(&path, &mode);
+	state.run();
 }
