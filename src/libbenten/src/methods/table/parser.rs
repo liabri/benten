251757d6b6a30d51f3c46d44affa93a0e5f5bb37
liabri <liabri@tuta.io>
2022-01-26 12:14:46 +0100

    use csv to parse tables/dicts

--- src/libbenten/src/methods/table/parser.rs
+++ src/libbenten/src/methods/table/parser.rs
@@ -2,17 +2,12 @@ use serde::Deserialize;
 use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
-use serde_with::{As, DisplayFromStr};
-use std::str::FromStr;
 
 use crate::BentenError;
 
 #[derive(Debug, PartialEq, Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct Table {
 	pub id: String,
-	pub encoding: String,
-	#[serde(with = "As::<Vec<DisplayFromStr>>")]
 	pub entries: Vec<Entry>
 }
 
@@ -22,24 +17,18 @@ pub struct Entry {
 	pub sequence: String,
 }
 
-impl FromStr for Entry {
-    type Err = std::num::ParseIntError;
-
-    fn from_str(entry: &str) -> Result<Self, Self::Err> {
-		let mut iter = entry.split_whitespace();
-
-    	let character: char = iter.next().unwrap().parse::<char>().unwrap();
-    	let sequence: String = iter.next().unwrap().to_string();
-
-        Ok(Entry { character, sequence })
-    }
-}
-
 impl Table {
 	pub fn new(id: &str, base_dir: &Path) -> Result<Table, BentenError> {
-		let path = base_dir.join("tables").join(id).with_extension("dict.yaml");
+		let path = base_dir.join("tables").join(id).with_extension("dict");
 		let file = File::open(path)?;
 	    let reader = BufReader::new(file);
-		Ok(serde_yaml::from_reader(reader)?)
+
+	    let mut entries: Vec<Entry> = csv::Reader::from_reader(reader).deserialize()
+	    	.map(|x| x.unwrap()).collect();
+
+		Ok(Self {
+			id: id.to_string(),
+			entries,
+		})
 	}
 }
