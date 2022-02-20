b352112e111e843ca557de3a7da69b7ee0c1f64d
lb06 <lbcat@tutanota.com>
2022-02-20 17:18:40 +0100

    got global parsing working

--- src/libbenten/src/methods/mod.rs
+++ src/libbenten/src/methods/mod.rs
@@ -1,5 +1,5 @@
 pub mod layout;
-use layout::{ Layout, LayoutMethod };
+use layout::{ Layout, LayoutMethod, LayoutKind };
 
 pub mod table;
 use table::{ Table, TableMethod };
@@ -31,22 +31,24 @@ pub struct Global {
 impl Global {
     pub fn new(id: &str, base_dir: &Path) -> Result<Self, BentenError> {
         let path = base_dir.join("layouts").join(id).with_extension("layout.zm");
-        let file = File::open(path)?;
+        let file = File::open(&path)?;
         let reader = BufReader::new(file);
 
-        match zmerald::from_reader::<_, Global>(reader) {
+        // Ok(zmerald::de::from_reader(reader)?)
+        match zmerald::de::from_reader::<_, Global>(reader) {
             Ok(mut g) => {
                 g.id = id.to_string();
                 return Ok(g);
             },
 
-            Err(_) => {
+            // this allows the config to define a single method not within a global struct, simpler single methods
+            Err(e) => {
                 if let Ok(table) = TableMethod::new(id, &base_dir) {
                     return Ok(Global::from(id, Box::new(table)));
                 } else {
                     match LayoutMethod::new(id, &base_dir) {
                         Ok(layout) => return Ok(Global::from(id, Box::new(layout))),
-                        Err(e) => return Err(e)
+                        Err(_) => panic!("{}", e)//return Err(BentenError::ZmeraldError(e))
                     }
                 }     
             }
@@ -68,20 +70,19 @@ impl Global {
 
 fn from_methods<'de, D>(deserializer: D) -> Result<Vec<Box<dyn GenericMethodTrait>>, D::Error>
 where D: Deserializer<'de> {
-    let values: Vec<zmerald::value::Value> = Vec::deserialize(deserializer)?;
+    let values: Vec<Layout> = Vec::deserialize(deserializer)?;
 
     let mut out: Vec<Box<dyn GenericMethodTrait>> = Vec::new();
     for value in values {
-        if let Ok(table) = value.clone().into_rust::<Table>() {
-            if let Ok(table_method) = TableMethod::try_from(table) {
-                out.push(Box::new(table_method));
-            }
-        } else {
-            match value.into_rust::<Layout>() {
-                Ok(layout) => out.push(Box::new(LayoutMethod::from(layout))),
-                Err(e) => return Err(serde::de::Error::custom(format!("error while parsing layout `{}`", e)))
-            }
-        } 
+        match value.kind {
+            LayoutKind::Layout => out.push(Box::new(LayoutMethod::from(value))),
+            LayoutKind::Table => {
+                if let Ok(table_method) = TableMethod::try_from(value) {
+                    out.push(Box::new(table_method));
+                }
+            },
+            LayoutKind::Hangeul => {}
+        }
     }
 
     Ok(out)
